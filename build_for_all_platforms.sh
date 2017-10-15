0fd34fc097c795528190e253ce49358496597530
Watson <watson1978@gmail.com>
2017-10-15 18:05:28 +0900

    fix

--- build_for_all_platforms.sh
+++ build_for_all_platforms.sh
@@ -3,8 +3,12 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-rbenv shell 2.0.0-p648
+rm -rf OBJROOT/.BS_INCLUDE.made
+rm -rf OBJROOT/.BS_RUBY.made
+rm -rf OBJROOT/.DSTROOT.made
 rm -rf DSTROOT
+
+rbenv shell 2.0.0-p648
 rm -rf OBJROOT/swig
 make
 
