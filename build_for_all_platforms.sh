831eb4dcbf81f363c4eb6e25d80c6b519075be32
Watson <watson1978@gmail.com>
2018-08-15 10:55:47 +0900

    cleanup

--- build_for_all_platforms.sh
+++ build_for_all_platforms.sh
@@ -3,14 +3,8 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-rm -rf OBJROOT/.BS_INCLUDE.made
-rm -rf OBJROOT/.BS_RUBY.made
-rm -rf OBJROOT/.DSTROOT.made
-rm -rf DSTROOT
-
 rbenv shell 2.0.0-p648
-rm -rf OBJROOT/swig
-make
+make rebuild
 
 rbenv shell 2.3.7
 rm -rf OBJROOT/swig
