a004c3a4cc50c840bb1505a45e3077f12d9f5874
Watson <watson1978@gmail.com>
2018-08-15 10:52:02 +0900

    Revert "delete because it does not work"
    This reverts commit 0489d496cfa87794e53df62453cdc73e974af067.

---
+++ build_for_all_platforms.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+rm -rf OBJROOT/.BS_INCLUDE.made
+rm -rf OBJROOT/.BS_RUBY.made
+rm -rf OBJROOT/.DSTROOT.made
+rm -rf DSTROOT
+
+rbenv shell 2.0.0-p648
+rm -rf OBJROOT/swig
+make
+
+rbenv shell 2.3.7
+rm -rf OBJROOT/swig
+make
