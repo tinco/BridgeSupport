9543bd35be5a86a34b8800614eb4a108b27f5a66
Tinco Andringa <mail@tinco.nl>
2018-10-08 19:07:26 +0200

    update test helper

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ require "xmlsimple"
 
 def gen_bridge_metadata(header_file_name, option = {})
   version = RUBY_VERSION.sub(/^(\d+\.\d+)(\..*)?$$/, "\\1")
-  command = "RUBYLIB='../DSTROOT/System/Library/BridgeSupport/ruby-#{version}' ruby ../gen_bridge_metadata.rb --format complete"
+  command = "RUBYLIB='../DSTROOT/System/Library/BridgeSupport/ruby-#{version}' ../ruby_wrapper ../gen_bridge_metadata.rb --format complete"
   output_path = "/tmp/bs_test.bridgesupport"
   full_command = "#{command} -c '-I. -I./header #{option[:cflags]}' ./header/#{header_file_name} -o #{output_path}"
   system full_command
